9a3108527f5883931032a2ed9e6ac8c7e37f6ad3
Tejeshkiran <tejeshkirant@gmail.com>
2024-06-21 18:29:04 +0530

    new changes from IST architect

--- framework-part/src/main/java/programResources/Devlopclass.java
+++ framework-part/src/main/java/programResources/Devlopclass.java
@@ -3,6 +3,7 @@ package programResources;
 public class Devlopclass {
 
     public static void main(String[] args) {
-        System.out.println("first commit from PST");
+        System.out.println("first commit from PST architect");
+        System.out.println("first time commit from IST architect");
     }
 }
